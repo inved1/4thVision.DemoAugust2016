f9837b4a83d1c24d0e350b969547da509f775b66
Daniel Inversini <inved1@bfh.ch>
2016-08-09 20:30:46 +0200

    some changes

---
+++ DemoAugust2016/Assets/Scripts/FlyMode.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyMode : MonoBehaviour {
+
+    private bool isFlying = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isFlying = !isFlying;
+            //var ri = gameObject.GetComponent<Rigidbody>();
+            //ri.useGravity = isFlying;
+            //ri.isKinematic  = isFlying;
+            SetAllCollidersStatus(isFlying);
+        }
+
+	}
+    public void SetAllCollidersStatus(bool active)
+    {
+        foreach (Collider c in GetComponents<Collider>())
+        {
+            c.enabled = active;
+            Debug.Log(c.enabled);
+        }
+    }
+}
